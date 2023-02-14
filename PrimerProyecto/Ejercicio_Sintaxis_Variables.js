0cfe76b5ad02b06ae1a659614de66bcc26fd0039
anpalmese <105017467+anpalmese@users.noreply.github.com>
2023-02-13 21:45:18 -0300

    Add files via upload

---
+++ PrimerProyecto/Ejercicio_Sintaxis_Variables.js
@@ -0,0 +1,9 @@
+let nombre = Augusto;
+let edad = 19;
+let desarrolador = true;
+let fecha_nacimiento = new Date (2003, 3, 27);
+const libro_favorito = {
+    nombre: "Percy Jackson y el Ladron del Rayo",
+    autor: "Rick Riordan",
+    fecha_lanzamiento :new Date (2005, 5, 08)
+}
